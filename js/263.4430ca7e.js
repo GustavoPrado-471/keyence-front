56168eb9eb5222ca6597750a1e89c576213ce2d2
GustavoPrado-471 <martinezgustavoalberto471@gmail.com>
2023-03-13 01:06:43 -0600

    New Deploy

---
+++ js/263.4430ca7e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkeyence_front"]=self["webpackChunkkeyence_front"]||[]).push([[263],{1263:function(n,e,t){t.r(e),t.d(e,{default:function(){return l}});t(7658);var u=t(3396),o=t(2483);const r=(0,u._)("h1",null,"NOT FOUND",-1);var c={__name:"NotFound",setup(n){const e=(0,o.tv)(),t=()=>{e.push("/logIn")};return(n,e)=>((0,u.wg)(),(0,u.iD)("div",null,[r,(0,u._)("button",{onClick:t},"Vover a Log In")]))}};const s=c;var l=s}}]);
+//# sourceMappingURL=263.4430ca7e.js.map
